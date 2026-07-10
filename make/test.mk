f1768eec72690bbe794e327c00adf205536e2360
Ilya Sitnikov <sitnikovik@gmail.com>
2026-07-10 19:35:52 +0300

    chore(makefile): add set -e to integration-test to exit on go test failure
    Added `set -e` to `make integration-test` to exit on `go test` failure. This ensures CI properly fails when integration tests fail.

--- make/test.mk
+++ make/test.mk
@@ -22,13 +22,17 @@ unit-test:
 integration-test:
 	@echo 🧪 Running integration tests...
 	@mkdir -p tmp
-	@coverpkgs=$$(go list ./... | grep -v 'internal/test/integration' | grep -v 'internal/test/fake' | tr '\n' ',' | sed 's/,$$//'); \
-		go test -tags=integration \
+	@set -e; \
+ 	coverpkgs=$$(go list ./... \
+		| grep -v 'internal/test/integration' \
+		| grep -v 'internal/test/fake' \
+		| tr '\n' ',' | sed 's/,$$//'); \
+ 	go test -tags=integration \
 		-race -count=1 -v \
 		-covermode=atomic \
 		-coverprofile=tmp/coverage_integration.out \
 		-coverpkg=$$coverpkgs \
 		./internal/test/integration/...; \
-		if [ -f tmp/coverage_integration.out ] && [ $$(wc -l < tmp/coverage_integration.out) -le 1 ]; then \
-			rm -f tmp/coverage_integration.out; \
-		fi
+	if [ -f tmp/coverage_integration.out ] && [ $$(wc -l < tmp/coverage_integration.out) -le 1 ]; then \
+		rm -f tmp/coverage_integration.out; \
+	fi
