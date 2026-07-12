8d69dc66a2506fd9b8ea41470c783d5cb4daaa1a
Ilya Sitnikov <sitnikovik@gmail.com>
2026-07-12 12:58:29 +0300

    feat(errors): check context error

--- errs/errors.go
+++ errs/errors.go
@@ -1,6 +1,9 @@
 package errs
 
-import "errors"
+import (
+	"context"
+	"errors"
+)
 
 var (
 	// ErrEmptyHash is returned when an empty commit hash is provided.
@@ -8,3 +11,12 @@ var (
 	// ErrGitFailed is returned when a git command execution fails.
 	ErrGitFailed = errors.New("git command failed")
 )
+
+// IsContextError checks if the provided error is a type of any context error.
+func IsContextError(err error) bool {
+	if err == nil {
+		return false
+	}
+	return errors.Is(err, context.Canceled) ||
+		errors.Is(err, context.DeadlineExceeded)
+}
